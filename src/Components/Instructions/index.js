1cf33e9f37c7cdf1fb9b651c4f9875eff626e070
Abhisek Malla <abhisek.malla16@gmail.com>
2024-08-07 09:55:23 +0530

    first push

---
+++ src/Components/Instructions/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Instructions() {
+  return (
+    <div>
+      <br /> <br />
+      <h1 style={{ textAlign: "center" }}>Information !!</h1>
+      <p
+        style={{
+          fontSize: "16px",
+          fontWeight: "400",
+          textAlign: "left",
+          paddingLeft: "8%",
+        }}
+      >
+        1. Use keyboard (⌨) <strong>ARROW</strong> keys to control your snake{" "}
+        <br />
+        2. Avoid <strong>crashing</strong> in walls 📓
+        <br />
+        3. 🏄 Speed of snake will <strong>increase</strong> with your{" "}
+        <strong>score</strong> <br />
+        4. If <strong>Game Over</strong> use <strong>ENTER/RETURN</strong> key
+        to restart 🔁
+        <br />
+        5. Use 💣💣<strong>ARROW Keys</strong> wisely as counter directional (↔)
+        keypress might kill ☠ your snake 🐍.
+      </p>
+    </div>
+  );
+}
+
+export default Instructions;
